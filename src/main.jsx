a8c40ec10eb21c7a0e5c07f281f3a1bfe25c00ee
Chloe Chan <hauyeeee@gmail.com>
2026-02-14 03:10:03 +0800

    Fix missing dependency react-is

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <App />
         </BrowserRouter>
       </LanguageProvider>
-    </HelmetProvider>
   </React.StrictMode>,
 );
