364424846dfa25e8514bf8f15f009d985efd5a84
Chloe Chan <hauyeeee@gmail.com>
2026-03-12 17:41:32 +0800

    改左啲野

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import CorporateBooking from './CorporateBooking.jsx'
 import './index.css' // 確保你有引入 Tailwind 嘅 CSS
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async' // 🔥 必須：為 SEO.jsx 提供 Context
